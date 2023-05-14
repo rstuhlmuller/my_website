0b414c2d76b868c0bf82a5f454c68908c66ec05f
Rodman <rodman@stuhlmuller.net>
2023-05-14 12:45:38 -0700

    fix: s3 path

--- IaC/modules/ui/s3.tf
+++ IaC/modules/ui/s3.tf
@@ -3,9 +3,9 @@ data "aws_s3_bucket" "personal_bucket" {
 }
 
 resource "aws_s3_object" "my_website" {
-  for_each = fileset("my_website/", "*")
+  for_each = fileset("../my_website/", "**")
   bucket   = data.aws_s3_bucket.personal_bucket.bucket
-  key      = each.value
-  source   = "my_website/${each.value}"
-  etag     = filemd5("my_website/${each.value}")
+  key      = "my_website/${each.value}"
+  source   = "../my_website//${each.value}"
+  etag     = filemd5("../my_website/${each.value}")
 }
