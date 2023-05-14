e5804dde8c1359fedaecde83a75fe48efcbdbbfd
Rodman <rodman@stuhlmuller.net>
2023-05-14 13:00:21 -0700

    fix: cloudfront

--- IaC/modules/ui/cloudfront.tf
+++ IaC/modules/ui/cloudfront.tf
@@ -1,7 +1,45 @@
-# resource "aws_cloudfront_distribution" "my_website" {
-#   origin {
-#     domain_name = data.aws_s3_bucket.personal_bucket.bucket_domain_name
-#     origin_id   = "my_website"
-#     origin_path = "/latest/site"
-#   }
-# }
+resource "aws_cloudfront_origin_access_identity" "my_website" {}
+
+resource "aws_cloudfront_distribution" "my_website" {
+  enabled = true
+
+  origin {
+    domain_name = data.aws_s3_bucket.personal_bucket.bucket_domain_name
+    origin_id   = "my_website"
+    origin_path = "/my_website"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.my_website.cloudfront_access_identity_path
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "my_website"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US"]
+    }
+  }
+  viewer_certificate {
+    acm_certificate_arn      = "arn:aws:acm:us-west-2:716182248480:certificate/996ff35d-8563-4927-8683-765b9143d384"
+    minimum_protocol_version = "TLSv1.2_2021"
+    ssl_support_method       = "sni-only"
+  }
+}
