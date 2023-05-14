186bf5a8dedfe8b488a2284ebe95370d88202448
Rodman <rodman@stuhlmuller.net>
2023-05-14 14:53:26 -0700

    fix: stuff

--- IaC/modules/ui/cloudfront.tf
+++ IaC/modules/ui/cloudfront.tf
@@ -14,11 +14,12 @@ resource "aws_cloudfront_distribution" "my_website" {
   enabled             = true
   default_root_object = "index.html"
   price_class         = "PriceClass_100"
+  wait_for_deployment = false
 
   origin {
-    domain_name = aws_s3_bucket.my_website.bucket_domain_name
+    domain_name = aws_s3_bucket.my_website.bucket_regional_domain_name
     origin_id   = "my_website"
-    origin_path = "my_website/"
+    origin_path = "/my_website"
 
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.my_website.cloudfront_access_identity_path
@@ -38,7 +39,7 @@ resource "aws_cloudfront_distribution" "my_website" {
       }
     }
 
-    viewer_protocol_policy = "allow-all"
+    viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
--- IaC/modules/ui/s3.tf
+++ IaC/modules/ui/s3.tf
@@ -2,19 +2,32 @@ resource "aws_s3_bucket" "my_website" {
   bucket = "aws-rstuhlmuller-s3-usw2-my-website"
 }
 
+locals {
+  mime_types = {
+    ".html" = "text/html"
+    ".css"  = "text/css"
+    ".js"   = "application/javascript"
+    ".ico"  = "image/vnd.microsoft.icon"
+    ".jpeg" = "image/jpeg"
+    ".png"  = "image/png"
+    ".svg"  = "image/svg+xml"
+  }
+}
+
 resource "aws_s3_object" "my_website" {
-  for_each = fileset("../my_website/", "**")
-  bucket   = aws_s3_bucket.my_website.bucket
-  key      = "my_website/${each.value}"
-  source   = "../my_website//${each.value}"
-  #   etag     = filemd5("../my_website/${each.value}")
+  for_each     = fileset("../my_website/", "**")
+  bucket       = aws_s3_bucket.my_website.bucket
+  key          = "my_website/${each.value}"
+  source       = "../my_website//${each.value}"
+  content_type = lookup(local.mime_types, regex("\\.[^.]+$", each.value), null)
+  etag         = filemd5("../my_website/${each.value}")
 }
 
 data "aws_iam_policy_document" "s3_policy" {
   statement {
     sid       = "my_website"
     actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.my_website.arn}/my_website/*"]
+    resources = ["${aws_s3_bucket.my_website.arn}/*"]
 
     principals {
       type        = "AWS"
