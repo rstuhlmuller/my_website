0e497c00fb8711ea7ce107fc3c2fc81047a1d721
Rodman <rodman@stuhlmuller.net>
2023-05-20 23:26:19 -0700

    feat: app

---
+++ my_website/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import AboutMe from './components/Main/AboutMe';
+import Navbar from './components/Navbar/Navbar';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <section>
+      </section>
+    </div>
+  );
+}
