f7305a03cc702d757377c793c7111d416e3b7c23
Rodman <rodman@stuhlmuller.net>
2023-05-21 17:25:49 -0700

    feat: typewriter

--- my_website/src/components/Main/AboutMe.js
+++ my_website/src/components/Main/AboutMe.js
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import profilePhoto from "./images/Profile.jpg"
 import "./AboutMeStyles.css"
+
+function Typewriter({ text }) {
+    const index = useRef(0);
+    const [currentText, setCurrentText] = useState("");
+
+    useEffect(() => {
+        index.current = 0;
+        setCurrentText("");
+    }, [text]);
+
+    useEffect(() => {
+        if (index.current < text.length) {
+            const timeoutID = setTimeout(() => {
+                setCurrentText((value) => value + text.charAt(index.current));
+            }, 30);
+            return () => {
+                index.current += 1;
+                clearTimeout(timeoutID);
+            };
+        };
+    }, [currentText, text]);
+    return <p id="Typewriter">{currentText}</p>;
+};
 
 export default function AboutMe() {
     return (
@@ -8,10 +31,8 @@ export default function AboutMe() {
             <div id="Introduction">
                 <h1>Rodman Stuhlmuller</h1>
                 <h2>DevOps Engineer</h2>
-                <p>
-                    Welcome to my personal website! I'm Rodman Stuhlmuller,
-                    a passionate and driven DevOps engineer based out of Seattle, WA.
-                </p>
+                <Typewriter text="Welcome to my personal website! I'm Rodman Stuhlmuller,
+                a passionate and driven DevOps engineer based out of Seattle, WA." />
             </div>
             <div id="ProfilePhoto">
                 <img alt="Profile" src={profilePhoto}></img>
