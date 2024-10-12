2028aa0e68220a2908f59bd91063af1fa20b172e
Rodman <rodman@stuhlmuller.net>
2024-10-11 23:07:11 -0700

    fix: rm cache

--- IaC/modules/ui/cloudfront.tf
+++ IaC/modules/ui/cloudfront.tf
@@ -18,7 +18,7 @@ resource "aws_cloudfront_distribution" "my_website" {
   origin {
     domain_name = aws_s3_bucket.my_website.bucket_regional_domain_name
     origin_id   = "my_website"
-    origin_path = "/my_website"
+    origin_path = "/my-website"
 
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.my_website.cloudfront_access_identity_path
